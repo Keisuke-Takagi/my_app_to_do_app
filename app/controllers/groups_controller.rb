29740d9adb4c68aad1435895f2ed760304b0e52c
Keisuke-Takagi <k19990410@gmail.com>
2019-06-15 17:12:58 +0900

    group, userのアソシエーション設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,20 +1,27 @@
 class GroupsController < ApplicationController
+  before_action :set_group
+  def index
+  end
+  
   def new
     @group = Group.new
     @group.users << current_user
   end
+
   def create
     @group = Group.new(group_params)
-    if @gorup.save
-      redirect_to root_path, 
+    if @group.save
+      
     else
-      render :newc
+      render :new
     end
   end
-  def index
-  end
+
   private
    def group_params
       params.require(:group).permit(:name, { :user_ids => [] })
+   end
+   def set_group
+      @group = Group.find(params[:id])
    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
   resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:new, :create, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
