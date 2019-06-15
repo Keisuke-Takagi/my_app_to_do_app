23cadade75d0f846aa7bf618992f73a12c1164ad
Keisuke-Takagi <k19990410@gmail.com>
2019-06-15 19:02:19 +0900

    データベースの削除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,22 +6,26 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
     @group.users << current_user
+
   end
 
   def create
     @group = Group.new(group_params)
+    binding.pry
     if @group.save
-      redirect_to   group_lists_path
+      redirect_to  root_path
     else
       render :new
     end
   end
 
+
   def edit
   end
   
-  def uodate
+  def update
   end
+
   private
    def group_params
       params.require(:group).permit(:name, { :user_ids => [] })
