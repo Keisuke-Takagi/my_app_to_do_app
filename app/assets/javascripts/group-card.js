ff41d04491da906356a012ace76cbfea3f9c94a7
Keisuke-Takagi <k19990410@gmail.com>
2019-07-02 18:49:21 +0900

    [WIP]カード追加ボタンのカスタムデータ属性の取得

--- app/assets/javascripts/group-card.js
+++ app/assets/javascripts/group-card.js
@@ -1,8 +1,20 @@
 $(document).on('turbolinks:load',function(){
   $(function(){
-    $(".task-box-wrapper__content__cards__add-button").on('click', function(){
-      $('.card-title-field').css('display', 'block');
-      
+
+    $(".task-box-wrapper__contentent__cards__add-button").on('click', function(){
+      var add_button_id = $(this).data("id")
+      debugger
+      $(this).css('display', 'none');
+
+      $(".card-title-field").css("display", "block")
+      $(".new-card-buttons__card-new-button").css("display", "block")
+      $(".new-card-buttons__remove-button").css("display", "block")
+    });
+    $(".new-card-buttons__remove-button").on("click", function(){
+      $(".new-card-buttons__remove-button").css("display", "none")
+      $(".new-card-buttons__card-new-button").css('display', 'none')
+      $(".card-title-field").css('display', 'none')
+      $(".task-box-wrapper__contentent__cards__add-button").css("display", "block")
     });
   });
 });
