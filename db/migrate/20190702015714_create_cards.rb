2b78e9b1d3a443734a1962f0ddb16420fac17c89
Keisuke-Takagi <k19990410@gmail.com>
2019-07-02 13:29:59 +0900

    cardmodelのマイグレーションファイルの編集

--- db/migrate/20190702015714_create_cards.rb
+++ db/migrate/20190702015714_create_cards.rb
@@ -3,6 +3,8 @@ class CreateCards < ActiveRecord::Migration[5.2]
     create_table :cards do |t|
       t.string :title
       t.text :discription
+      t.references :group, foreign_key: true, null: false 
+      t.references :list, foreign_key: true
       t.timestamps
     end
   end
