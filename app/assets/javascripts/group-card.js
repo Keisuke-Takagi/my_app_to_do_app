fea52d80988da09a8ab25c9b7e7b5a3df6631b11
Keisuke-Takagi <k19990410@gmail.com>
2019-07-03 20:56:39 +0900

    非同期でcard入力フォームの表示

--- app/assets/javascripts/group-card.js
+++ app/assets/javascripts/group-card.js
@@ -2,19 +2,26 @@ $(document).on('turbolinks:load',function(){
   $(function(){
 
     $(".task-box-wrapper__contentent__cards__add-button").on('click', function(){
-      var add_button_id = $(this).data("id")
-      debugger
+      var add_button_wrapper = $(this).parent();
+      var add_text_area = $(add_button_wrapper).find(".card-title-field");
+      var add_button = $(add_button_wrapper).find(".new-card-buttons__card-new-button")
+      var remove_button = $(add_button_wrapper).find(".new-card-buttons__remove-button")
+      $(add_text_area).css("display", "block")
+      $(add_button).css("display", "block")
+      $(remove_button).css("display", "block")
       $(this).css('display', 'none');
-
-      $(".card-title-field").css("display", "block")
-      $(".new-card-buttons__card-new-button").css("display", "block")
-      $(".new-card-buttons__remove-button").css("display", "block")
     });
     $(".new-card-buttons__remove-button").on("click", function(){
-      $(".new-card-buttons__remove-button").css("display", "none")
-      $(".new-card-buttons__card-new-button").css('display', 'none')
-      $(".card-title-field").css('display', 'none')
-      $(".task-box-wrapper__contentent__cards__add-button").css("display", "block")
+      var this_new_card_buttons = $(this).parent();
+      var add_button_wrapper = $(this_new_card_buttons).parent();
+      var card_button_wrapper = $(add_button_wrapper).parent();
+      var add_text_area = $(add_button_wrapper).find(".card-title-field")
+      var add_button = $(add_button_wrapper).find(".new-card-buttons__card-new-button")
+      var new_button_add = $(card_button_wrapper).find(".task-box-wrapper__contentent__cards__add-button")
+      $(this).css("display", "none")
+      $(add_text_area).css('display', "none")
+      $(add_button).css('display', 'none')
+      $(new_button_add).css("display", "block")
     });
   });
 });
