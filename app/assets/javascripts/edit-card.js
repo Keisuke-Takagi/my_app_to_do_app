d5a00093e11917eb46bd9284c7d044a94468a1e4
Keisuke-Takagi <k19990410@gmail.com>
2019-07-09 18:40:20 +0900

    move card right function

--- app/assets/javascripts/edit-card.js
+++ app/assets/javascripts/edit-card.js
@@ -1,14 +1,11 @@
 $(document).on('turbolinks:load',function(){
   $(function(){
     $(".card-icons__next").on("click", function(){
-      var this_page_move_forms = $(".move-card-form")
+      // var this_page_move_forms = $(".move-card-form")
+      var this_page_move_forms = $(".task-box-wrapper__contentent__cards")
       var parent_1 = $(this).parent();
       var parent_2 = $(parent_1).parent();
-      var list_id = $(this).data("id")
       var card_id__hidden = $(parent_2).find(".card-id__hidden").val();
-      var card_title__hidden = $(parent_2).find(".card-title__hidden").val();
-      var card_discription__hidden = $(parent_2).find(".card-discription__hidden").val();
-      var card_list_id__hidden = $(parent_2).find(".card-list_id__hidden");
       var move_card__form = $(parent_2).find(".move-card-form");
       var this_form_data_id = $(move_card__form).data("id")
       $.each(this_page_move_forms, function(index, a_form){
@@ -16,7 +13,7 @@ $(document).on('turbolinks:load',function(){
         if(each_list_id >  this_form_data_id){
           sessionStorage.setItem("list_id_next", each_list_id)
           return false
-        }
+        };
       });
       var list_id_next = Number(sessionStorage.getItem("list_id_next"))
       var $dir = location.href.split("/");  
@@ -26,10 +23,8 @@ $(document).on('turbolinks:load',function(){
       $(parent_2).find(".card-list_id__hidden").attr("value", list_id_next)
     });
     $(".edit-card-button").on("click", function(){
-
     })
     $(".move-card-form").on("submit", function(){
-
     });
     $(".edit-card-form").on("submit", function(){
       return false
