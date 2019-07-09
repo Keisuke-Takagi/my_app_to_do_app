42232d9afde3de0f6d6ad4fe95e3f81d26be63e2
Keisuke-Takagi <k19990410@gmail.com>
2019-07-09 19:09:56 +0900

    move card left function

--- app/assets/javascripts/edit-card.js
+++ app/assets/javascripts/edit-card.js
@@ -7,7 +7,7 @@ $(document).on('turbolinks:load',function(){
       var parent_2 = $(parent_1).parent();
       var card_id__hidden = $(parent_2).find(".card-id__hidden").val();
       var move_card__form = $(parent_2).find(".move-card-form");
-      var this_form_data_id = $(move_card__form).data("id")
+      var this_form_data_id = $(move_card__form).data("id");
       $.each(this_page_move_forms, function(index, a_form){
         var each_list_id = $(a_form).data("id");
         if(each_list_id >  this_form_data_id){
@@ -21,6 +21,33 @@ $(document).on('turbolinks:load',function(){
       var url = "/groups/" + $dir2 + "/lists/" + list_id_next + "/cards/" +  card_id__hidden
       move_card__form.attr("action", url)
       $(parent_2).find(".card-list_id__hidden").attr("value", list_id_next)
+    });
+    $(".card-icons__previous").on("click", function(){
+      var this_page_move_forms = $(".task-box-wrapper__contentent__cards")
+      this_page_move_forms.sort(function(a,b){
+        if($(a).data("id") > $(b).data("id")) return -1;
+        if($(a).data("id") < $(b).data("id") ) return 1;
+        return 0;
+      });
+      var parent_1 = $(this).parent();
+      var parent_2 = $(parent_1).parent();
+      var card_id__hidden = $(parent_2).find(".card-id__hidden").val();
+      var move_card__form = $(parent_2).find(".move-card-form");
+      var this_form_data_id = $(move_card__form).data("id");
+      debugger
+      $.each(this_page_move_forms, function(index, a_form){
+        var each_list_id = $(a_form).data("id");
+        if(each_list_id <  this_form_data_id){
+          sessionStorage.setItem("list_id_previous", each_list_id)
+          return false
+        };
+      });
+      var list_id_previous = Number(sessionStorage.getItem("list_id_previous"))
+      var $dir = location.href.split("/");  
+      var $dir2 = $dir[$dir.length -1];
+      var url = "/groups/" + $dir2 + "/lists/" + list_id_previous + "/cards/" +  card_id__hidden
+      move_card__form.attr("action", url)
+      $(parent_2).find(".card-list_id__hidden").attr("value", list_id_previous)
     });
     $(".edit-card-button").on("click", function(){
     })
