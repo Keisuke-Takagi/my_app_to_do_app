9c76973d97ddb6bb6cc9962f427f4521bed00138
Keisuke-Takagi <k19990410@gmail.com>
2019-07-19 18:00:37 +0900

    Add validation

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   belongs_to :group
   has_many :cards,  dependent: :delete_all
   validates :text, presence: true
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :name, length: {in: 1..6}
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :group_users
