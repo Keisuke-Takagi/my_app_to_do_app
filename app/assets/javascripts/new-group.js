9be7d661c8e287bd1e14bc637cff3269f1586500
Keisuke-Takagi <k19990410@gmail.com>
2019-07-22 13:16:27 +0900

    listタイトルが改行されるよう修正

--- app/assets/javascripts/new-group.js
+++ app/assets/javascripts/new-group.js
@@ -1,14 +1,2 @@
 $(document).on('turbolinks:load',function(){
-  //   $("#group-form-create").validate({
-  //     rules: {
-  //       user_ids: {
-  //         required: true
-  //       }
-  //     },
-  //     messages: {
-  //       user_ids: {
-  //         required: swal ("参加するユーザーを選択して下さい", "" ,"error")
-  //       }
-  //     }
-  // })
 })
