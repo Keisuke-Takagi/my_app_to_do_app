dd1b7d5a9d4739fdf2581fc676cf081b10dbef11
Keisuke-Takagi <k19990410@gmail.com>
2019-07-22 12:56:50 +0900

    Adjustment groups#new view

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 class Group < ApplicationRecord
-  validates :name, presence: true 
   has_many :group_users
   has_many :users, through: :group_users
   has_many :lists
   has_many :cards
-  # validates :name, presence: true
+  validates :user_ids, presence: true
+  validates :name, presence: true
 end
