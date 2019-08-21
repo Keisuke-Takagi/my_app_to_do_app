6f99ff0e01ef4a4d651cb752cf05c9ca3d87408d
Keisuke-Takagi <k19990410@gmail.com>
2019-08-21 14:48:48 +0900

    Capistaranoで作成したファイルの編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,31 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "my_app_to_do_app"
+set :repo_url, "git@github.com:Keisuke-Takagi/my_app_to_do_app"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
+set :rbenv_type, :user
+set :rbenv_ruby,'2.5.1'
 
+set :ssh_options, auth_methods: ['publickey'],
+keys: ['~/.ssh/tesh-key.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
